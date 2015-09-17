b7d9cd63aaa6b49e1ce76cd69e5623fc2deefc28
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-09-17 17:37:07 -0300

    adicionado status no dwarf

--- src/modulo-01-java-OO/Modulo01/Dwarf.java
+++ src/modulo-01-java-OO/Modulo01/Dwarf.java
@@ -1,11 +1,22 @@
 public class Dwarf {
-    private int vida = 110;
+    private int vida;
+    private Status status;
     
     public void receberFlechada() {
         this.vida -= 10;
+        if(this.vida <= 0){
+            this.status=Status.MORTO;
+        }
     }
     
     public int getVida() {
         return this.vida;
+    }
+    public Status getStatus(){
+        return this.status;
+    }
+    public Dwarf(){
+        this.status=Status.VIVO;
+        this.vida=110;
     }
 }
