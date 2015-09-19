3c9945028e9279d351f894731d2b6117a15e99d5
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-09-19 13:59:22 -0300

    Exercicio 1: Métodos criados e testes para os mesmos.

--- src/modulo-01-java-OO/Modulo01/Inventario.java
+++ src/modulo-01-java-OO/Modulo01/Inventario.java
@@ -1,12 +1,30 @@
 import java.util.ArrayList;
 public class Inventario
 {
-    ArrayList<Item> listaitens=new ArrayList<>();
+    private ArrayList<Item> listaItens;
     
-    public void adicionarItem(Inventario item){
-        
+    public Inventario(){
+        this.listaItens=new ArrayList<Item>();
     }
-    public void perderItem(Inventario item){
-        
+    public ArrayList<Item> getListaItens(){
+        return listaItens;
+    }
+    public void adicionarItem(Item item){
+        listaItens.add(item);
+    }
+    public void perderItem(Item item){
+        listaItens.remove(item);
+    }
+    public String getDescricoesItens(){
+        int tamanho=getListaItens().size();
+        StringBuilder strBuilder = new StringBuilder();
+        for(int x=0;x<tamanho;x++){
+            if(x<(tamanho-1)){
+                strBuilder.append(getListaItens().get(x).getDescricao()).append(",");
+            }else{
+                strBuilder.append(getListaItens().get(x).getDescricao());
+            }
+        }
+        return strBuilder.toString();
     }
 }
---
+++ src/modulo-01-java-OO/Modulo01/InventarioTest.java
@@ -0,0 +1,56 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+
+
+public class InventarioTest
+{
+    @Test
+    public void inventarioCriadoEAdicionaItem(){
+        Inventario lista=new Inventario();
+        Item item=new Item("escudo",3);
+        Item item1=new Item("poçao",1);
+        lista.adicionarItem(item);
+        lista.adicionarItem(item1);
+
+        assertEquals(lista.getListaItens().get(0),item);
+        assertEquals(lista.getListaItens().get(1),item1);
+    }
+    @Test
+    public void inventarioCriadoERemoveItem(){
+        Inventario lista=new Inventario();
+        Item item=new Item("escudo",3);
+        Item item1=new Item("espada",5);
+        Item item2=new Item("mapa",2);
+        
+        lista.adicionarItem(item);
+        lista.adicionarItem(item1);
+        lista.perderItem(item1);
+        lista.adicionarItem(item2);
+        
+        assertEquals(lista.getListaItens().get(0),item);
+        assertEquals(lista.getListaItens().get(1),item2);
+        assertEquals(2,lista.getListaItens().size());
+    }
+    @Test
+    public void inventarioGetDescricoesItens(){
+        Inventario lista=new Inventario();
+        Item item=new Item("escudo",2);
+        Item item1=new Item("espada",1);
+        Item item2=new Item("poçao",5);
+        Item item3=new Item("mapa",2);
+        String esperado="escudo,espada,poçao,mapa";
+        
+        lista.adicionarItem(item);
+        lista.adicionarItem(item1);
+        lista.adicionarItem(item2);
+        lista.adicionarItem(item3);
+        
+        assertEquals(esperado,lista.getDescricoesItens());
+    }
+    
+}
