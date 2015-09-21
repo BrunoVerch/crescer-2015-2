6d5593b5e8f61466964bb22dbe52933176b8f902
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-09-21 17:37:32 -0300

    Acrescentado método

--- src/modulo-01-java-OO/Modulo01/Dwarf.java
+++ src/modulo-01-java-OO/Modulo01/Dwarf.java
@@ -1,10 +1,10 @@
 public class Dwarf {
-    private int vida;
-    private Status status;
-    private String nome;
-    private int experiencia;
-    private DataTerceiraEra dataNascimento;
-    private Inventario inventario;
+    protected int vida;
+    protected Status status;
+    protected String nome;
+    protected int experiencia;
+    protected DataTerceiraEra dataNascimento;
+    protected Inventario inventario;
     
     public Dwarf() {
         this.vida = 110;
@@ -74,5 +74,6 @@ public class Dwarf {
             }
         }
     }
+        
     
 }
