819771dab4abbb273ba94db31e5d6c0840541df7
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-09-22 21:15:51 -0300

    refactor(Dwarf) conforme herança

--- src/modulo-01-java-OO/Modulo01/Dwarf.java
+++ src/modulo-01-java-OO/Modulo01/Dwarf.java
@@ -1,10 +1,5 @@
 public class Dwarf extends Personagens{
-    protected int vida;
-    protected Status status;
-    protected String nome;
-    protected int experiencia;
     protected DataTerceiraEra dataNascimento;
-    protected Inventario inventario;
     
     public Dwarf() {
         super();
@@ -12,38 +7,21 @@ public class Dwarf extends Personagens{
         this.dataNascimento = new DataTerceiraEra(1,1,1);
     }
     public Dwarf(String nome) {
-        super(nome);
-        this.vida = 110;
-        this.dataNascimento = new DataTerceiraEra(1,1,1);
+        this();
+        this.nome=nome;
     }
     public Dwarf(String nome, DataTerceiraEra dataNascimento) {
-        super(nome);
+        this(nome);
         this.dataNascimento = dataNascimento;
-    }
-    public Inventario getInventario(){
-        return this.inventario;
-    }
-    public int getVida() {
-        return this.vida;
-    }
-    public Status getStatus(){
-        return this.status;
     }
     public DataTerceiraEra getDataNascimento(){
         return this.dataNascimento;
     }
-    public String getNome(){
-        return this.nome;
+    /*
+    public void atacarOrc(Orc orc){
+        orc.levarAtaqueDeAnao();
     }
-    public int getExperiencia(){
-        return this.experiencia;
-    }
-    public void adicionarItem(Item item) {
-        this.inventario.adicionarItem(item);
-    }
-    public void perderItem(Item item) {
-        this.inventario.perderItem(item);
-    }
+    */
     public Double getNumeroSorte(){
         double valorInicial=101.0;
         if(this.dataNascimento.ehBissexto() && (this.vida >= 80 && this.vida <=90)){
@@ -58,23 +36,26 @@ public class Dwarf extends Personagens{
         return valorInicial;
     }
     public void receberFlechada() {
-            if(this.status==Status.VIVO){
                 if(this.getNumeroSorte() < 0){
                     this.experiencia += 2;
                 } else if(this.getNumeroSorte()>100){
-                     this.perderVida();  
+                    
+                    if(this.vida >= 10){
+                        this.vida -= 10;
+                    }
+                    if(this.vida == 0){
+                        this.status=Status.MORTO;
+                    }
                 }
-            }
+            
     }
     public void perderVida(){
-        if(this.status==Status.VIVO){
             if(this.vida >= 10){
                 this.vida -= 10;
             }
             if(this.vida == 0){
                 this.status=Status.MORTO;
             }
-        }
     }
     public void tentarSorte() {
         double numero = getNumeroSorte();
