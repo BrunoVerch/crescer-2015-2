1c77e9d7cfda6c5aec88a18a355b74f60969064d
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-09-22 21:26:46 -0300

    feat(ElfoVerde) implementa classe elfo verde

--- src/modulo-01-java-OO/Modulo01/ElfoVerde.java
+++ src/modulo-01-java-OO/Modulo01/ElfoVerde.java
@@ -1,6 +1,22 @@
 
 
-public class ElfoVerde
+public class ElfoVerde extends Elfo
 {
-    
+    public ElfoVerde(String nome, int flechas) {
+        super(nome, flechas);
+    }
+    public ElfoVerde(String nome) {
+        super(nome);
+    }
+    public void atirarFlecha(Dwarf dwarf) {
+        super.atirarFlecha(dwarf);
+        this.experiencia++;
+    }
+    public void adicionarItem(Item item) {
+        boolean podeAdicionar = item != null && (item.getDescricao().equals("Espada de aço valiriano") || 
+                                 item.getDescricao().equals("Arco e flecha de vidro"));
+        if (podeAdicionar) {
+            super.adicionarItem(item);
+        }
+    }
 }
