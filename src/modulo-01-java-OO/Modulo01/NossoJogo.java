8c834c46d587e7f492bd7f8c08da96bbb7e235ba
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-09-24 17:26:23 -0300

    feat(NossoJogo) implementar main

---
+++ src/modulo-01-java-OO/Modulo01/NossoJogo.java
@@ -0,0 +1,12 @@
+
+public class NossoJogo
+{
+    public static final String NOME="Bahh of the rings";
+    
+    public static void main(String[] args){
+        if(args.length > 0){
+            System.out.println("Olá "+args[0]+", bem vindo ao "+ NOME);
+        }
+        
+    }
+}
