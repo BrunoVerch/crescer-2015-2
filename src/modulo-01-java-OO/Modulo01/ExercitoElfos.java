dd3c1656df3c3ecacf568044b670686c30864ad6
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-09-24 00:14:14 -0300

    feat(ExercitoElfos) adicionar agrupar e buscar por status, getters

--- src/modulo-01-java-OO/Modulo01/ExercitoElfos.java
+++ src/modulo-01-java-OO/Modulo01/ExercitoElfos.java
@@ -1,12 +1,16 @@
-import java.util.HashMap;
+import java.util.*;
 public class ExercitoElfos
 {
-    HashMap<String,Elfo> exercito;
+    HashMap<String, Elfo> exercito;
+    HashMap<Status, ArrayList<Elfo>> grupoStatus;
     public ExercitoElfos(){
         this.exercito=new HashMap<>();
     }
-    public HashMap getHashMap(){
+    public HashMap<String,Elfo> getExercito(){
         return exercito;
+    }
+    public HashMap<Status,ArrayList<Elfo>> getGrupoStatus(){
+        return grupoStatus;
     }
     public void alistarElfo(Elfo elfo){
         if(elfo instanceof ElfoVerde || elfo instanceof ElfoNoturno){
@@ -15,5 +19,22 @@ public class ExercitoElfos
     }
     public Elfo buscarElfo(Elfo elfo){
         return exercito.get(elfo.getNome());
+    }
+    public void agruparPorStatus(){
+        grupoStatus=new HashMap<>();
+        //for each _____values retorna coleção de valores
+        for(Elfo elfo : exercito.values()){
+            if(!grupoStatus.containsKey(elfo.getStatus())){
+                grupoStatus.put(elfo.getStatus(),new ArrayList<Elfo>());
+            }
+            grupoStatus.get(elfo.getStatus()).add(elfo);
+        }
+    }
+    public ArrayList<Elfo> buscar(Status status){
+        return grupoStatus.get(status);
+    }
+    public boolean equals(Object obj) {
+        ExercitoElfos outro = (ExercitoElfos)obj;
+        return this.exercito.equals(outro.getExercito());
     }
 }
