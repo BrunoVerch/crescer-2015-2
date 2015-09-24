6b1ebe6f3ca6973c364bad0c39deb2153a5ca1e3
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-09-24 00:15:22 -0300

    test(ExercitoElfos) adicionar três cenarios

--- src/modulo-01-java-OO/Modulo01/ExercitoElfosTest.java
+++ src/modulo-01-java-OO/Modulo01/ExercitoElfosTest.java
@@ -17,7 +17,7 @@ public class ExercitoElfosTest
         
         exercito.alistarElfo(elfo);
         
-        assertEquals(esperado,exercito.getHashMap());
+        assertEquals(esperado,exercito.getExercito());
     }
     @Test
     public void exercitoCriadoAlistaElfoNoturno(){
@@ -28,7 +28,7 @@ public class ExercitoElfosTest
         
         exercito.alistarElfo(elfo);
         
-        assertEquals(esperado,exercito.getHashMap());
+        assertEquals(esperado,exercito.getExercito());
     }
     @Test
     public void exercitoCriadoAlistaElfoEBuscaElfo(){
@@ -39,5 +39,52 @@ public class ExercitoElfosTest
         Elfo buscaElfo=exercito.buscarElfo(elfo);
         
         assertEquals(elfo,buscaElfo);
+    }
+    @Test
+    public void exercitoCriadoAlistaElfosEAgrupaPorStatus(){
+        ExercitoElfos exercito=new ExercitoElfos();
+        ElfoNoturno elfo=new ElfoNoturno("elfo");
+        ElfoVerde elfo1=new ElfoVerde("elfo1");
+        exercito.alistarElfo(elfo);
+        exercito.alistarElfo(elfo1);
+        ExercitoElfos esperado=new ExercitoElfos();
+        esperado.alistarElfo(elfo);
+        esperado.alistarElfo(elfo1);
+        esperado.agruparPorStatus();
+        
+        
+        exercito.agruparPorStatus();
+        
+        assertEquals(esperado,exercito);
+    }
+    @Test
+    public void umElfoMortoAgruparPorStatus(){
+       ExercitoElfos exercitoDeElfos = new ExercitoElfos();
+       ElfoNoturno noturno = new ElfoNoturno("Noturno", 100);
+       for(int i = 0; i < 99; i++){
+           noturno.atirarFlecha(new Dwarf());
+       }
+       exercitoDeElfos.alistarElfo(noturno);
+       exercitoDeElfos.agruparPorStatus();
+       assertEquals(noturno, exercitoDeElfos.buscar(Status.MORTO).get(0));
+    }
+    @Test
+    public void tresElfosMortosEAgrupadosPorStatus(){
+        ExercitoElfos exercito = new ExercitoElfos();
+        ElfoNoturno noturno = new ElfoNoturno("Noturno 1", 100);
+        ElfoNoturno noturno2 = new ElfoNoturno("Noturno 2", 100);
+        ElfoNoturno noturno3 = new ElfoNoturno("Noturno 3", 100);
+        for(int i = 0; i < 99; i++){
+           noturno.atirarFlecha(new Dwarf());
+           noturno2.atirarFlecha(new Dwarf());
+           noturno3.atirarFlecha(new Dwarf());
+       }
+        exercito.alistarElfo(noturno);
+        exercito.alistarElfo(noturno2);
+        exercito.alistarElfo(noturno3);
+        exercito.agruparPorStatus();
+        assertEquals(noturno, exercito.buscar(Status.MORTO).get(0));
+        assertEquals(noturno2, exercito.buscar(Status.MORTO).get(1));
+        assertEquals(noturno3, exercito.buscar(Status.MORTO).get(2));
     }
 }
