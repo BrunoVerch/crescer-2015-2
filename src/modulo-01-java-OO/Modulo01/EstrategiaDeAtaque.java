b53204d2f526ef964baa04e010df30777e529c56
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-09-26 20:53:41 -0300

    feat(EstrategiaDeAtaque,EstrategiaNormal) adicionar ordemDoUltimoAtaque

--- src/modulo-01-java-OO/Modulo01/EstrategiaDeAtaque.java
+++ src/modulo-01-java-OO/Modulo01/EstrategiaDeAtaque.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 public interface EstrategiaDeAtaque
 {
+    ArrayList<Elfo> ordemDoUltimoAtaque();
     void atacar(ExercitoElfos exercito, ArrayList<Dwarf> dwarves);
 }
