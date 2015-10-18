9c912267b27ce34760f13f04afa0af8033047e01
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-10-18 18:04:41 -0200

    feat(lista-js) implementar método maiorTexto

--- src/modulo-03-web/dia-01/lista-js-basico.js
+++ src/modulo-03-web/dia-01/lista-js-basico.js
@@ -6,4 +6,16 @@ function daisyGame(numPetalas){
 		console.log('love me');
 	}
 }
+/*exercicio 2*/
+function maiorTexto(arrayStrings){
+	var maior=arrayStrings[0].length;
+	var palavra=arrayStrings[0];
+	for (var i = 0; i < arrayStrings.length; i++) {
+		if(arrayStrings[i].length > maior){
+			maior=arrayStrings[i].length;
+			var palavra=arrayStrings[i];
+		}
+	}
+	return palavra;
+}
 
