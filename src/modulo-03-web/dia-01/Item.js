9e6e948013ea61fc5a779708832918e93daf500e
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-10-21 21:53:35 -0200

    reimplementar item

---
+++ src/modulo-03-web/dia-01/Item.js
@@ -0,0 +1,9 @@
+function Item(sku,descricao, quantidade, valorUnitario){
+  this.sku=sku;
+  this.descricao=descricao;
+  this.quantidade=quantidade;
+  this.valorUnitario=valorUnitario;
+};
+Item.prototype.subTotal = function(){
+  return this.quantidade * this.valorUnitario;
+};
