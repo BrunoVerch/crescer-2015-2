2ebc6d6ba6c01deadab89570927f1a88feb2438a
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-10-20 23:37:05 -0200

    feat(adicionarItem,removerItem,index) implementar funções para adicionar e remover itens do carrinho

--- src/modulo-03-web/dia-01/CarrinhoDeCompras.js
+++ src/modulo-03-web/dia-01/CarrinhoDeCompras.js
@@ -1,3 +1,15 @@
 function CarrinhoDeCompras(){
   this.carrinho=[];
 };
+CarrinhoDeCompras.prototype.adicionarItem = function (itens) {
+  this.carrinho.push(itens);
+};
+
+CarrinhoDeCompras.prototype.removerItem = function (sku) {
+  this.carrinho.splice(this.index(sku),1);
+};
+CarrinhoDeCompras.prototype.index = function(sku){
+  return this.carrinho.map(function(elem) { 
+    return elem.sku; 
+   }).indexOf(sku);
+};
