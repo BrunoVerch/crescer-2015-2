e0965ac43373e3af8c5e30686a0ff95848a819e1
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-10-21 17:33:08 -0200

    feat(forcarCompra,concluirPedido) implementar setInterval

--- src/modulo-03-web/dia-01/CarrinhoDeCompras.js
+++ src/modulo-03-web/dia-01/CarrinhoDeCompras.js
@@ -9,8 +9,8 @@ CarrinhoDeCompras.prototype.removerItem = function (sku) {
   this.carrinho.splice(this.index(sku),1);
 };
 CarrinhoDeCompras.prototype.index = function(sku){
-  return this.carrinho.map(function(elem) { 
-    return elem.sku; 
+  return this.carrinho.map(function(elem) {
+    return elem.sku;
    }).indexOf(sku);
 };
 
@@ -22,13 +22,30 @@ CarrinhoDeCompras.prototype.valorTotal = function () {
   var total = this.carrinho.reduce(function (value,elem) {
     return value + elem.subTotal();
   },0);
-  return this.sortear(40) ? total*0.9 : total;
+  return this.sortear(40) ? Math.floor(total*0.9) : total;
 };
 
 CarrinhoDeCompras.prototype.sortear = function(chance){
   if(Math.random()*100 < chance ){
     return true;
-  } else { 
+  } else {
     return false;
   }
+};
+
+CarrinhoDeCompras.prototype.forcarCompra = function () {
+  if(!this.intervalo){
+    var self=this;
+    //guarda o id em uma propriedade do objeto
+    this.intervalo = setInterval(function () {
+      self.carrinho.forEach(function (elem) {
+        elem.valorUnitario=elem.valorUnitario*1.1;
+      })
+    },5000);
+  };
+}
+
+CarrinhoDeCompras.prototype.concluirPedido = function () {
+  clearInterval(this.intervalo);
+  delete this.intervalo;
 };
