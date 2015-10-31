51511b750a827380ec40ddefcdcbbab6cb387a81
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-10-31 02:39:15 -0200

    feat(getId,getJogo,EditarJogo) implementar métodos para editar o jogo

--- src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Dominio/BibliotecaDeJogos.cs
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Dominio/BibliotecaDeJogos.cs
@@ -37,5 +37,46 @@ namespace Locadora.Dominio
             return listaRetorno;
         }
 
+        public int getId(string nome)
+        {
+            XElement arquivoXML = XElement.Load(caminhoDoArquivo);
+            XElement jogoEsperado = arquivoXML.Element("jogos").Elements().FirstOrDefault(jogo => nome == jogo.Element("nome").Value);
+            return Convert.ToInt32(jogoEsperado.Attribute("id").Value);   
+        }
+
+        private XElement getJogo(int id)
+        {
+            XElement arquivoXML = XElement.Load(caminhoDoArquivo);
+            return arquivoXML.Element("jogos").Elements().FirstOrDefault(jogo => id == Convert.ToInt32(jogo.Attribute("id").Value));
+        }
+
+        public void EditarJogo(int id, Jogo jogo)
+        {
+            XElement jogoXML = getJogo(id);
+            jogoXML.Element("nome").SetValue(jogo.Nome);
+            jogoXML.Element("preco").SetValue(jogo.Preco.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture));
+            jogoXML.Element("categoria").SetValue(jogo.Categoria);
+        }
+
+        public void EditarNomeDoJogo(int id,string nomeAlterado)
+        {
+            XElement jogo = getJogo(id);
+            jogo.Element("nome").SetValue(nomeAlterado);
+            jogo.Save(caminhoDoArquivo);
+        }
+
+        public void EditarPrecoDoJogo(int id, double novoValor)
+        {
+            XElement jogo = getJogo(id);
+            jogo.Element("preco").SetValue(novoValor.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture));
+            jogo.Save(caminhoDoArquivo);
+        }
+
+        public void EditarCategoriaDoJogo(int id, string categoria)
+        {
+            XElement jogo = getJogo(id);
+            jogo.Element("categoria").SetValue(categoria);
+            jogo.Save(caminhoDoArquivo);
+        }
     }
 }
