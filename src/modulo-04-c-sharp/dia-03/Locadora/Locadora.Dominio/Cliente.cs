ae2a7dadb2f3815d83ad0209236676ca36e50ac0
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-10-31 18:40:58 -0200

    modificado modificador de cliente, modificações do visual studio

--- src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Dominio/Cliente.cs
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Dominio/Cliente.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Locadora.Dominio
 {
-    class Cliente
+    public class Cliente
     {
         public int Id { get; private set; }
         public string Nome { get; private set; }
