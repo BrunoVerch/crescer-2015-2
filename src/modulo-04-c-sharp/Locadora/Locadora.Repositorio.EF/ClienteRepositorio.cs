e6c510774cc6f624560c9ff41c265442128d40e2
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-11-10 00:14:01 -0200

    feat(ClienteRepositorio) implementar repositorio do cliente

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/ClienteRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using Locadora.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Locadora.Dominio;
+
+namespace Locadora.Repositorio.EF
+{
+    public class ClienteRepositorio : IClienteRepositorio
+    {
+        public IList<Cliente> BuscarPorNome(string nome)
+        {
+            using (var db = new CodeFirst())
+            {
+                return db.Cliente.Where(p => p.Nome == nome).ToList();
+            }
+        }
+    }
+}
