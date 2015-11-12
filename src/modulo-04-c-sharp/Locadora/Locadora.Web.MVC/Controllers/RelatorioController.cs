b1540cd240c28eeadf8b62887634445d11af3f3f
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-11-12 03:22:52 -0200

    feat(RelatorioController) implementar autocomplete

--- src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Controllers/RelatorioController.cs
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Controllers/RelatorioController.cs
@@ -1,4 +1,6 @@
-﻿using Locadora.Dominio.Repositorio;
+﻿using Locadora.Dominio;
+using Locadora.Dominio.Repositorio;
+using Locadora.Web.MVC.Helpers;
 using Locadora.Web.MVC.Models;
 using Locadora.Web.MVC.Seguranca;
 using System;
@@ -18,18 +20,12 @@ namespace Locadora.Web.MVC.Controllers
         {
             repositorio = CriarJogoRepositorio();
             var model = new RelatorioModel();
-            IList<Dominio.Jogo> ListaJogos=new List<Dominio.Jogo>();            
-            if(!string.IsNullOrWhiteSpace(nomeJogo))
-            {
-                ListaJogos = repositorio.BuscarPorNome(nomeJogo);
-            }
-            else
-            {
-                ListaJogos = repositorio.BuscarTodos();
-            }            
+            IList<Dominio.Jogo> ListaJogos=new List<Dominio.Jogo>();
+            ListaJogos = ObterJogosPorFiltro(nomeJogo);
+                       
             foreach (var jogo in ListaJogos)
             {
-                var jogoModel = new JogoModel() { Id = jogo.Id, Nome = jogo.Nome, Categoria = jogo.Categoria.ToString() };
+                var jogoModel = new JogoModel() { Id = jogo.Id, Nome = jogo.Nome, Categoria = jogo.Categoria.ToString(), Selo=jogo.Selo.ToString() };
                 model.Jogos.Add(jogoModel);
             }
             if (ListaJogos.Count == 0)
@@ -39,6 +35,29 @@ namespace Locadora.Web.MVC.Controllers
             model.QuantidadeDeJogos = model.Jogos.Count;
                
             return View(model);
+        }
+
+        public JsonResult JogosAutocomplete(string term)
+        {
+            IList<Jogo> jogosEncontrados = ObterJogosPorFiltro(term);
+
+            var json = jogosEncontrados.Select(x => new { label = x.Nome });
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        private IList<Jogo> ObterJogosPorFiltro(string nome)
+        {
+            IJogoRepositorio jogoRepositorio = FabricaDeModulos.CriarJogoRepositorio();
+
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                return jogoRepositorio.BuscarTodos();
+            }                
+            else
+            {
+                return jogoRepositorio.BuscarPorNome(nome);
+            }                
         }
     }
 }
