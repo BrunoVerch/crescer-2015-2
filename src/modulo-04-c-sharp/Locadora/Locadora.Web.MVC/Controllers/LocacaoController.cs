1ca8fc6f352734bc8c2e7dcceb66bb53dde9b16e
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-11-12 03:34:33 -0200

    feat(LocarJogo) comecar implementacao de locar

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Controllers/LocacaoController.cs
@@ -0,0 +1,27 @@
+﻿using Locadora.Dominio.Repositorio;
+using Locadora.Dominio;
+using Locadora.Web.MVC.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Locadora.Web.MVC.Models;
+
+namespace Locadora.Web.MVC.Controllers
+{
+    public class LocacaoController : Controller
+    {
+        private IJogoRepositorio repositorio = null;
+
+        public ActionResult Locar(int id)
+        {
+            repositorio = FabricaDeModulos.CriarJogoRepositorio();           
+            Jogo jogo = repositorio.BuscarPorId(id);
+
+            var model = new LocarJogoModel() { Id = jogo.Id, Nome = jogo.Nome, Descricao = jogo.Descricao, Imagem = jogo.Imagem, Selo = jogo.Selo };
+
+            return View(model);
+        }
+    }
+}
---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Models/LocarJogoModel.cs
@@ -0,0 +1,26 @@
+﻿using Locadora.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Web.MVC.Models
+{
+    public class LocarJogoModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Imagem { get; set; }
+
+        public string Descricao { get; set; }
+
+        public Selo Selo { get; set; }
+
+        public DateTime DataDevolucao { get; set; }
+
+        public decimal Valor { get; set; }
+    }
+}
