03ecacdd1592d06e079ab254cdf67ddbd71f1986
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-11-13 04:40:51 -0200

    test(Jogo,Usuario) adicionar testes para usuario e jogo

--- src/modulo-04-c-sharp/Locadora/Locadora.Dominio.Test/JogoTest.cs
+++ src/modulo-04-c-sharp/Locadora/Locadora.Dominio.Test/JogoTest.cs
@@ -16,13 +16,56 @@ namespace Locadora.Dominio.Test
         }
 
         [TestMethod]
+        public void JogoADeveSerDiferenteJogoB()
+        {
+            Jogo jogoA = new Jogo(id: 1);
+            Jogo jogoB = new Jogo(id: 2);
+
+            Assert.AreNotEqual(jogoA, jogoB);
+        }
+
+        [TestMethod]
         public void LocacaoParaClienteTemIdCorreto()
         {
             Jogo jogo = new Jogo();
 
-            jogo.LocarPara(new Cliente(id: 1));
+            jogo.LocarPara(new Cliente(1));
 
-            Assert.AreEqual(1, jogo.ClienteLocacao);
+            Assert.AreEqual(1, jogo.ClienteLocacao.Id);
+        }
+
+        [TestMethod]
+        public void ClienteDevolveJogo()
+        {
+            Jogo jogo = new Jogo();
+            jogo.LocarPara(new Cliente(1));
+            jogo.DevolverJogo();
+
+            Assert.AreEqual(null, jogo.ClienteLocacao);
+        }
+
+        [TestMethod]
+        public void JogoSeloOuroBuscaValor()
+        {
+            Jogo jogo = new Jogo()
+            {
+                Selo = Selo.OURO
+            };
+            decimal valor=jogo.Valor;
+
+            Assert.AreEqual(15, valor);
+        }
+
+        [TestMethod]
+        public void JogoSeloBronzeBuscaValor()
+        {
+            Jogo jogo = new Jogo()
+            {
+                Selo = Selo.BRONZE
+            };
+            decimal valor = jogo.Valor;
+
+            Assert.AreEqual(5, valor);
         }
     }
 }
---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Dominio.Test/UsuarioTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Dominio.Test
+{
+    [TestClass]
+    public class UsuarioTest
+    {
+        [TestMethod]
+        public void UsuariosCriadoComEmailIgual()
+        {
+            Usuario usuario = new Usuario()
+            {
+                Email = "abc@abc.com",
+                Senha = "12345"
+            };
+            Usuario usuario2 = new Usuario()
+            {
+                Email = "abc@abc.com",
+                Senha = "12235"
+            };
+
+            Assert.AreEqual(usuario.Email, usuario2.Email);
+        }
+
+        [TestMethod]
+        public void UsuarioAdicionaPermissao()
+        {
+            Usuario usuario = new Usuario()
+            {
+                Email = "abc@abc.com",
+                Senha = "12345"
+            };
+            usuario.AdicionarPermissao(new Permissao("ADMIN"));
+
+            Assert.AreEqual("ADMIN",usuario.Permissoes.First().Nome);
+        }
+
+        [TestMethod]
+        public void UsuarioVerificaSeTemPermissaoSucesso()
+        {
+            Usuario usuario = new Usuario()
+            {
+                Email = "abc@abc.com",
+                Senha = "12345"
+            };
+            usuario.AdicionarPermissao(new Permissao("ADMIN"));
+
+            Assert.IsTrue(usuario.TemPermissao("ADMIN"));
+        }
+
+        [TestMethod]
+        public void UsuarioVerificaSeTemPermissaoFracassa()
+        {
+            Usuario usuario = new Usuario()
+            {
+                Email = "abc@abc.com",
+                Senha = "12345"
+            };
+            usuario.AdicionarPermissao(new Permissao("ADMIN"));
+
+            Assert.IsFalse(usuario.TemPermissao("MASTER"));
+        }
+    }
+}
