58a713cf0f80211b3a228f7817a8677962bfaaf9
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-11-13 04:44:17 -0200

    feat(JogoMap) acrescentar propriedades

--- src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/Mapping/JogoMap.cs
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/Mapping/JogoMap.cs
@@ -20,9 +20,11 @@ namespace Locadora.Repositorio.EF.Mapping
             Property(p => p.Descricao).IsRequired().HasColumnName("Descricao").HasMaxLength(255);
             Property(p => p.Imagem).IsOptional().HasColumnName("Imagem").HasMaxLength(255);
             Property(p => p.Video).IsOptional().HasColumnName("Video").HasMaxLength(255);
-            Property(p => p.DataDevolucao).IsOptional().HasColumnName("DataDevolucao");
+            Property(p => p.DataLocacao).IsOptional().HasColumnName("DataDevolucao");
+            Property(p => p.Categoria).IsRequired().HasColumnName("Categoria");
+            Property(p => p.Selo).IsRequired().HasColumnName("Selo");
 
-            HasOptional(p => p.ClienteLocacao).WithOptionalDependent().Map(m => m.MapKey("IdClienteLocacao"));
+            HasOptional(p => p.ClienteLocacao).WithMany().HasForeignKey(x => x.IdCliente);
         }
     }
 }
