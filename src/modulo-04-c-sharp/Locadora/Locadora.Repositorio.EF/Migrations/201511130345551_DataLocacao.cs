9061afe9c85617562aec6f930eb07fdaf0c7ef0a
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-11-13 04:42:23 -0200

    feat(Migrations) adicionar migrations

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/Migrations/201511130345551_DataLocacao.cs
@@ -0,0 +1,16 @@
+namespace Locadora.Repositorio.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataLocacao : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
