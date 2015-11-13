399470d777066e81397ca56460f8abcd12c693cd
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-11-13 04:43:17 -0200

    feat(Model) implementar JogoDevolucao e ListaJogos Model

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Models/JogoDevolucaoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Web.MVC.Models
+{
+    public class JogoDevolucaoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataLocacao { get; set; }
+        public decimal ValorTotal { get; set; }
+    }
+}
---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Models/ListaJogosModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Web.MVC.Models
+{
+    public class ListaJogosModel
+    {
+        public int IdCliente { get; set; }
+        public List<JogoDevolucaoModel> Jogos { get; set; }
+
+        public ListaJogosModel()
+        {
+            Jogos = new List<JogoDevolucaoModel>();
+        }
+    }
+}
