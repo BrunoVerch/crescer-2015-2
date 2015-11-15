8568cc3b0db9b08092f1655ee3bc52112368c304
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-11-14 22:27:19 -0200

    feat(Locadora) adicionar cadastrar usuario

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Models/UsuarioModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Web.MVC.Models
+{
+    public class UsuarioModel
+    {
+        [Required]
+        [StringLength(200)]
+        public string NomeCompleto { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        [EmailAddress(ErrorMessage = "Email invalido")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Senha { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string ConfirmarSenha { get; set; }
+    }
+}
