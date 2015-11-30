90e9158e284acfe5356cb3fcc51038369e3e10cf
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-11-30 02:30:26 -0200

    adicao de busca por nome do cliente

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/controller/ClienteController.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/controller/ClienteController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -42,6 +43,12 @@ public class ClienteController {
     public ModelAndView viewExibe(@PathVariable("id") Long id) {
         return new ModelAndView("cliente/exibe", "cliente", clienteService.buscarClientePorId(id));
     }
+    
+    @PreAuthorize("hasRole('ADMIN')")
+    @RequestMapping(path = "/buscar", method = RequestMethod.GET)
+	public ModelAndView buscar(@RequestParam("nome") String nome){
+		return new ModelAndView("cliente/lista", "clientes", clienteService.buscarClientePorNome(nome));
+	}
 
     @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(path = "/editar/{id}", method = RequestMethod.GET)
